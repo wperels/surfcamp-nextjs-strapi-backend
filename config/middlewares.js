1581682eacc94f8f021a3f298eb44f43879692ad
Surfcamp Dev <dev@surfcamp.local>
2026-02-07 16:14:23 -0700

    Add Cloudinary upload provider

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,7 +1,30 @@
 module.exports = [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'connect-src': ["'self'", 'https:'],
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com', // Cloudinary CDN
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'res.cloudinary.com',
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
