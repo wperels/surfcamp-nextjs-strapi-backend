bb5133e09fab9d5e700d3ee3044b07700d62761e
Surfcamp Dev <dev@surfcamp.local>
2026-02-07 15:05:52 -0700

    fixed config/server.js after Render deploy failed

--- config/server.js
+++ config/server.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   //port: env.int('PORT', 1337),
    port: env.int('PORT', 10000),
-   url: env('PUBLIC_URL')
+  url: env('PUBLIC_URL'),
   app: {
     keys: env.array('APP_KEYS'),
   },
