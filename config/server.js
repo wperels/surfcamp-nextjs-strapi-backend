3e820b6773ebb66ecc5ccf7cc08d7812b19b7abb
Surfcamp Dev <dev@surfcamp.local>
2026-02-07 13:05:17 -0700

    updated database.js and server.js in prep for Render

--- config/server.js
+++ config/server.js
@@ -1,6 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  //port: env.int('PORT', 1337),
+   port: env.int('PORT', 10000),
+   url: env('PUBLIC_URL')
   app: {
     keys: env.array('APP_KEYS'),
   },
